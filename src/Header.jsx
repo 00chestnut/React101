3c874f1f48f82e797edc9dfe34f7d03620a32b5a
00chestnut <maksymilian@witoszek.com>
2025-03-04 12:50:41 +0100

    Work on 101

---
+++ src/Header.jsx
@@ -0,0 +1,32 @@
+function Header(){
+
+    return(
+        <header>
+            <h1>
+                My website 
+            </h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">Work</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">Time</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#">Dollops</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+
+};
+
+export default Header;
