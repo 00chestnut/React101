ae0fa109fb6cc08a588be7df1901f866d4012107
00chestnut <maksymilian@witoszek.com>
2025-03-04 12:59:49 +0100

    Deletions

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+
 function App() {
-return(
-  <>
-  <Header/>
-  <Foote/>
-  <Food/>
-  </>
-);
 }
 
 export default App
