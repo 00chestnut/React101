d337470ccdb1a4d7171d3221c1f27ba7306ab950
00chestnut <maksymilian@witoszek.com>
2025-03-05 09:20:06 +0100

    Fixes:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Card from './components/Card.jsx'
 function App() {
+  return(
   <div>
   <Card />
   </div>
+  );
 };
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-import Picture from './assets/kasztany.png'
+import Picture from '../assets/kasztany.png'
 function Card() {
   return (
     <div className="card">
       <img src={Picture} alt="Kasztany"></img>
       <h2>Maksymilian</h2>
-      <p>Świetny programista</p>
+      <p>Great programmer</p>
     </div>
   );
 }
