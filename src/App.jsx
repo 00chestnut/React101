c63dfa006a18d88e3ec52966adb121e96eaac33a
00chestnut <maksymilian@witoszek.com>
2025-03-05 11:41:08 +0100

    Better css settings for images in Cards. +1 Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Card from "./components/Card.jsx";
-import Picture from "./assets/LH44.jpg"
+import hamimg from "./assets/LH44.jpg"
+import verimg from "./assets/verstappen.jpeg"
 function App() {
   return (
     <>
       <Card
-        picture={Picture}
+        picture={hamimg}
         name="Lewis Hamilton"
         championships="7"
         alt="Lewis Hamilton"
+      />
+      <Card 
+        picture={verimg}
+        name="Max Verstappen"
+        championships="4"
+        alt="Max Verstappen"
+
       />
     </>
   );
