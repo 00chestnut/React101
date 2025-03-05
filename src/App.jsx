e0d5569766f721a7069c718457ec61751847cfef
00chestnut <maksymilian@witoszek.com>
2025-03-05 14:25:17 +0100

    New fresh animations Bonus: Schweiz Car

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Card from "./components/Card.jsx";
-import hamimg from "./assets/LH44.jpg"
-import verimg from "./assets/verstappen.jpeg"
+import hamimg from "./assets/LH44.jpg";
+import verimg from "./assets/verstappen.jpeg";
+import szwaimg from "./assets/szwajc.png";
+/* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-= */
 function App() {
   return (
     <>
@@ -10,15 +12,17 @@ function App() {
         desc="7x Formula 1 World Champion"
         alt="Lewis Hamilton"
       />
-      <Card 
+      <Card
         picture={verimg}
         name="Max Verstappen"
         desc="4x Formula 1 World Champion"
         alt="Max Verstappen"
-
       />
+      <Card picture={szwaimg}
+        name="Schweiz"
+        desc="Idiot"/>
       <Card />
-      </>
+    </>
   );
 }
 export default App;
