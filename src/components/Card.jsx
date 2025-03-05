1905e0be86d6ca550fde3cd4077caa2e98a86623
00chestnut <maksymilian@witoszek.com>
2025-03-05 09:58:24 +0100

    Added Lewis Hamilton Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-import Picture from '../assets/kasztany.png'
+import Picture from '../assets/LH44.jpg'
 function Card() {
   return (
     <div className="card">
-      <img src={Picture} alt="Kasztany"></img>
-      <h2>Maksymilian</h2>
-      <p>Great programmer</p>
+      <img src={Picture} alt="Lewis Hamilton Celebrating" className="cardImg"></img>
+      <h2>Lewis Hamilton</h2>
+      <p>7x Formula 1 World Champion</p>
     </div>
   );
 }
