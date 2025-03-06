3a0e2410565f420942b7899c8888deb344a4a20c
00chestnut <maksymilian@witoszek.com>
2025-03-06 10:22:48 +0100

    Button work

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,32 @@
-
-    function Button(){
-        return(
-            <>
-            <br/>
-            <button className="button">Click me gng💔</button>
-            </>
-        );
+function Button() {
+    
+  let clicks = 0;
+  const handleClick = () => {
+    if (clicks < 2) {
+      clicks++;
+      console.log("You diggin in me");
+    } else if (clicks < 3) {
+      clicks++;
+      console.log("Slow down");
+    } else {
+      console.log("Stop clicking please", { clicks });
+      clicks++;
     }
+  };
 
-    export default Button;
+  function Dig() {
+    const parameters = (e) => console.log(e);
+    return <button onClick={(e) => parameters(e)}> </button>;
+  }
+  return (
+    <>
+      <br />
+      <button className="button" onClick={handleClick}>
+        Click me gng💔
+      </button>
+      {Dig()}
+    </>
+  );
+}
+
+export default Button;
